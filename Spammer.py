39058257a029582f96ff408ab03eedcd7035191b
LuikCastro <luik.silva@bandtec.com.br>
2021-03-07 18:06:24 -0300

    padrão

--- Spammer.py
+++ Spammer.py
@@ -1,5 +1,5 @@
 import pyautogui, time
 time.sleep(4)
 for c in range(100):
-    pyautogui.typewrite("je suis venus victrix")
+    pyautogui.typewrite("Spam bot message")
     pyautogui.press("enter")

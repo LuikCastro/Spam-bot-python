26a11ef05591eeea55c2974356a075776729f319
LuikCastro <luik.silva@bandtec.com.br>
2021-03-10 11:56:58 -0300

    atualizando spam bot

--- Spammer.py
+++ Spammer.py
@@ -1,5 +1,5 @@
 import pyautogui, time
 time.sleep(4)
 for c in range(100):
-    pyautogui.typewrite("Spam bot message")
+    pyautogui.typewrite("Seculai se cudon ameno")
     pyautogui.press("enter")
